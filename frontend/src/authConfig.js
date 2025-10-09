3ff3b93364829a7de62a66c169631df4804a6cb6
Mari Kumagai <m.kumagai@mki-us.com>
2025-10-08 20:12:04 -0400

    authConfig.js updated

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,12 +1,14 @@
+const isLocalhost = window.location.hostname === "localhost";
+
 export const msalConfig = {
   auth: {
     clientId: "7f5409d9-622c-4701-bafe-53c09a018607",
     authority: "https://login.microsoftonline.com/cb6120fa-a878-459e-969b-e04700bccd48",
-    redirectUri: "http://localhost:3000"
+    redirectUri: isLocalhost
+      ? "http://localhost:3000"
+      : "https://yellow-rock-0b8f9f20f.1.azurestaticapps.net"
   },
-  cache: { cacheLocation: "sessionStorage" }
-};
-
-export const loginRequest = {
-  scopes: ["openid", "profile", "email", "api://<BACKEND_CLIENT_ID>/access_as_user"]
+  cache: {
+    cacheLocation: "sessionStorage"
+  }
 };
