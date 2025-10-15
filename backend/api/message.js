b44e03d046184c7d0365a76b1510413f2839c575
m.kumagai@mki-us.com <m.kumagai@mki-us.com>
2025-10-15 19:57:46 -0400

    update message.js

--- backend/api/message.js
+++ backend/api/message.js
@@ -1,9 +1,14 @@
 const { app } = require('@azure/functions');
 
 app.http('message', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        return { body: JSON.stringify({ "text": `Hello, from the API!` }) };
-    }
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: async (request, context) => {
+    return {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: { text: "Hello, from the API!" }
+    };
+  }
 });
