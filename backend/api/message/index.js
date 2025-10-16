c0030e78ad93057ea4bd5cb43bbb2a1c6db2c3ca
m.kumagai@mki-us.com <m.kumagai@mki-us.com>
2025-10-15 21:17:46 -0400

    update API

--- backend/api/message/index.js
+++ backend/api/message/index.js
@@ -1,7 +1,7 @@
 const { app } = require('@azure/functions');
 
 app.http('message', {
-  methods: ['GET', 'POST'],
+  methods: ['GET'],
   authLevel: 'anonymous',
   handler: async (request, context) => {
     return {
